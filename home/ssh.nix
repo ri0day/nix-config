1803e40583da2b53da8923ae19937c9f5c426199
mason.wu <email@me.com>
2024-05-04 18:07:10 +0800

    new arch

---
+++ home/ssh.nix
@@ -0,0 +1,18 @@
+{...}: {
+  programs.ssh {
+  enable = true;
+  matchBlocks = {
+  "devopszoo" = {
+    hostname = "100.84.196.17";
+    user = "ubuntu";
+    identityFile = "~/.ssh/id_rsa";
+  };
+  "github.com" = {
+  hostname = "github.com";
+  user = "git";
+  proxyCommand = "nc -x localhost:7897 %h %p";
+
+  };
+  };
+
+}
