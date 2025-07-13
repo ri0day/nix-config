01f7508b150d53191db19f88fcd78529eeb8bb1e
mason.wu <email@me.com>
2025-07-13 23:35:39 +0800

    secret

--- home/secrets.nix
+++ home/secrets.nix
@@ -4,13 +4,9 @@ config,
 }: {
   sops = {
     age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
+    defaultSopsFile = ./test-sec.yaml;
     secrets.hello = {
-    sopsFile = "/tmp/test-sec.yaml";
        path = "/tmp/mason-hello.txt";
-      };
-    secrets."nested/level1" = {
-    sopsFile = "/tmp/test-sec.yaml";
-       path = "/tmp/mason-nested-level1.txt";
       };
     };
   }
